f3db46aec5996b23450bb54e725b35adabb7eda9
saparbaeva016 <gulshiragulshira938@.gmail.com>
2025-05-09 12:39:53 +0300

    my favourite site

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import homepage,detail
+from .views import homepage,detail,category_funk
 
 urlpatterns=[
     path('',homepage,name='home'),
     path('news/<int:id>/',detail,name='detail'),
+    path('category/<int:id>/',category_funk,name='category')
 ]
--- news/views.py
+++ news/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render,get_object_or_404
-from .models import News 
+from .models import News , Category
 
 
 
 def homepage(request):
     news=News.objects.all()
-    return render(request,'home.html',{'news':news})
+    categories=Category.objects.all()
+    return render(request,'home.html',{'news':news,'categories':categories})
 
 
 def detail(request,id):
     news=get_object_or_404(News,id=id)
     return render(request,'detail.html',{'abc':news})
+
+def category_funk(request,id):
+    categories=Category.objects.all()
+    news=News.objects.filter(category_id=id)
+    return render(request,'category.html',{'news':news,'categories':categories})
+
+
 
 
 
